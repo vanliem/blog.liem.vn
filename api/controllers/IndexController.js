de4d0811e0cd372970c1c044c0bc35b3489647ca
vanliem <vanliem2204@gmail.com>
2017-07-20 13:59:14 +0700

    prepare HomeController

---
+++ api/controllers/IndexController.js
@@ -0,0 +1,13 @@
+/**
+ * IndexControllerController
+ *
+ * @description :: Server-side logic for managing Indexcontrollers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	index: function (req, res) {
+	  //http://jsoneditoronline.org/?id=af90232ca200bceb7df211f37cd103ad
+  }
+};
+
